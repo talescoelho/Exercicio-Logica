2212401c0463ed59e674bbe6256451362fe8b0c1
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-29 14:25:49 -0300

    Update exercicio4.js

--- LOGICA/exercicio4.js
+++ LOGICA/exercicio4.js
@@ -1,6 +1,6 @@
 const mercado = require('./mercado');
 
-// escreva a função frutaPredominante para retornar um objeto com o nome da fruta que tem mais meses predominantes; 
+// escreva a função frutaPredominante para retornar o nome da fruta que tem mais meses predominantes; 
 function frutaPredominante() {
   
 }
