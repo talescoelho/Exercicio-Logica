7e33338bbd3531df01371807b0850c0bba6d59e6
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-10-04 11:10:38 -0300

    passo 6

--- LOGICA/exercicio3.js
+++ LOGICA/exercicio3.js
@@ -2,7 +2,17 @@ const mercado = require('./mercado');
 
 // complete a função maisCara para retornar os dados da fruta com o maior preço;
 function maisCara() {
-  
-}
+    const market = mercado.frutas;
+    let counter = 0;
+    let frutaCara;
+    for (let index = 0; index < market.length; index += 1) {
+    if (counter < market[index].preco) {
+    counter = market[index].preco;
+    frutaCara = market[index];
+    }
+    }
+    return frutaCara;
+    }
+    console.log(maisCara())
 
 module.exports = maisCara;
--- LOGICA/exercicio6.js
+++ LOGICA/exercicio6.js
@@ -1,8 +1,13 @@
 const mercado = require('./mercado');
 
 // complete a função buscaPorNome, que ao receber o nome da fruta, retorna um objeto com os dados da fruta
-function buscaPorNome() {
-  
-}
-
+function buscaPorNome(nameFruit) {
+    const market = mercado.frutas;
+    for (let index = 0; index < market.length; index += 1) {
+    if (market[index].nome === nameFruit) {
+    return market[index];
+    }
+    }
+    }
+    console.log(buscaPorNome('Manga'))
 module.exports = buscaPorNome;
