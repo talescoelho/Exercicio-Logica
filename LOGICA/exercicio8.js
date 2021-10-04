28430e3e0061ed036b273d720efb8f4dc0ba2ac5
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-10-04 11:24:08 -0300

    passo 8

--- LOGICA/exercicio8.js
+++ LOGICA/exercicio8.js
@@ -2,8 +2,17 @@ const mercado = require('./mercado');
 
 // complete a função buscaValorNutritivo, que ao receber um valor nutritivo como parametro,
 // retorna um array com o nome de todas as frutas que possui esse valor nutritivo
-function buscaValorNutritivo() {
-
+function buscaValorNutritivo(vitamina) {
+    const market = mercado.frutas;
+    const arrayFruits = [];
+    for (let i = 0; i < market.length; i += 1) {
+      let fruits = market[i]['valorNutritivo'];
+      // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+      if (fruits.includes(vitamina)) {
+        arrayFruits.push(market[i]['nome']);
+      }
+    }
+    return arrayFruits;
 }
-
+console.log(buscaValorNutritivo('B'));
 module.exports = buscaValorNutritivo;
