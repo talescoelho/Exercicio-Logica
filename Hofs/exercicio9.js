78e03dfaa75d443be0c4d472a0de98019c0b634a
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-10-07 18:08:58 -0300

    Update exercicio9.js

--- Hofs/exercicio9.js
+++ Hofs/exercicio9.js
@@ -4,20 +4,20 @@ const produtos = require('./produtos');
 Neste exercício você vai precisar retornar um objeto que tem as seguintes características:
 {
   NOME_LOJA_1: {
-    mousse: [NOME_DOS_MOUSES_QUE_EXISTEM_NA_LOJA_1],
+    mouse: [NOME_DOS_MOUSES_QUE_EXISTEM_NA_LOJA_1],
     keyboards: [NOME_DOS_TECLADOS_QUE_EXISTEM_NA_LOJA_1],
     headsets: [NOME_DOS_HEADSETS_QUE_EXISTEM_NA_LOJA_1],
     graphicsCard: [NOME_DAS_PLACAS_DE_VÍDEO_QUE_EXISTEM_NA_LOJA_1]
   },
   NOME_LOJA_2: {
-    mousse: [NOME_DOS_MOUSES_QUE_EXISTEM_NA_LOJA_2],
+    mouse: [NOME_DOS_MOUSES_QUE_EXISTEM_NA_LOJA_2],
     keyboards: [NOME_DOS_TECLADOS_QUE_EXISTEM_NA_LOJA_2],
     headsets: [NOME_DOS_HEADSETS_QUE_EXISTEM_NA_LOJA_2],
     graphicsCard: [NOME_DAS_PLACAS_DE_VÍDEO_QUE_EXISTEM_NA_LOJA_2]
   }
   ...
   NOME_LOJA_N: {
-    mousse: [NOME_DOS_MOUSES_QUE_EXISTEM_NA_LOJA_N],
+    mouse: [NOME_DOS_MOUSES_QUE_EXISTEM_NA_LOJA_N],
     keyboards: [NOME_DOS_TECLADOS_QUE_EXISTEM_NA_LOJA_N],
     headsets: [NOME_DOS_HEADSETS_QUE_EXISTEM_NA_LOJA_N],
     graphicsCard: [NOME_DAS_PLACAS_DE_VÍDEO_QUE_EXISTEM_NA_LOJA_N]
