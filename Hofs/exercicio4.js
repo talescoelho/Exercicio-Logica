350c74bc40a2c6d89c5a83b8bceabd1483c192c7
Tales Coelho dos Santos <tales-coelho@live.com>
2021-10-07 17:21:58 -0300

    Update exercicio4.js

--- Hofs/exercicio4.js
+++ Hofs/exercicio4.js
@@ -4,14 +4,7 @@ const produtos = require('./produtos');
 // retornar o nome de todos os produtos que tenham a diferença entre
 // o preço máximo e o preço mínimo maior que o número informado
 // caso não exista, retornar um array vazio
-function differenceBetweenMaxAndMinPrice(num) {
-  const arr = Object.values(produtos)
-  const newObj = [...arr[0], ...arr[1], ...arr[2], ...arr[3]].filter(({ prices }) => Math.max(...prices) - Math.min(...prices) > num);
-  return newObj.map(({ name }) => name);
+function differenceBetweenMaxAndMinPrice() {
 } 
-
-console.log(
-  differenceBetweenMaxAndMinPrice(300)
-)
 
 module.exports = differenceBetweenMaxAndMinPrice;
