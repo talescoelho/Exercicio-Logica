f79104f075e1274e8457343377914fb29922c09e
Tales Coelho dos Santos <tales-coelho@live.com>
2021-10-07 13:27:35 -0300

    hofs

---
+++ Hofs/produtos.js
@@ -0,0 +1,50 @@
+const produtos = {
+  mouses: [
+    {
+      id: "M001",
+      name: "Mouse Gamer Cobra Chroma M711 Rgb Redragon",
+      brand: "Redragon",
+      dpi: 10.000,
+      color: ["black", "white"],
+      bluetooth: false,
+      buttons: 8,
+      stores: ["KaBuM!", "Terabyteshop", "Pichau"],
+      prices: [114.90, 116.90, 129.90],
+    },
+    {
+      id: "M002",
+      name: "Mouse Gamer Logitech G203 Rgb Lightsync Preto",
+      brand: "Logitech ",
+      dpi: 8.000,
+      color: ["black", "white"],
+      bluetooth: false,
+      buttons: 6,
+      stores: ["KaBuM!", "Terabyteshop", "Kalunga"],
+      prices: [119.90, 129.90, 161.90],
+    },
+  ],
+  keyboards: [
+    {
+      id: "K001",
+      name: "",
+      brand: "",
+      color: [""],
+      bluetooth: true,
+      stores: ["", "", ""],
+      prices: [, , ],
+    },
+  ],
+  headset: [
+    {
+      id: "H001",
+      name: "",
+      brand: "",
+      color: [""],
+      bluetooth: true,
+      stores: ["", "", ""],
+      prices: [, , ],
+    },
+  ],
+};
+
+module.exports = produtos;
