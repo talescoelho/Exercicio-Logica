ae939822988e10a39ba4feb2417e1c5dd576b955
Tales Coelho dos Santos <tales-coelho@live.com>
2021-10-07 16:00:37 -0300

    exercicio 6

--- Hofs/exercicio5.js
+++ Hofs/exercicio5.js
@@ -3,7 +3,7 @@ const produtos = require('./produtos');
 // complete a função para que ela receba o nome de uma loja e o produto como parâmetro e retorne
 // um array com o nome de todos os produtos daquela loja. se não houver nem um produto retornar um array
 // vazio
-function verifyStore() {
+function verifyStore(store, product) {
 
 }
 
--- Hofs/testes/test.js
+++ Hofs/testes/test.js
@@ -1,6 +1,7 @@
 const verifyStore = require('../exercicio5');
+const verifyId = require('../exercicio6');
 
-describe("Exercício 5 Função nomeFrutas()", () => {
+describe("Exercício 5 Função verifyStore", () => {
   it("verifica se a função verifyStore('KaBuM!', 'keyboards') retorna um array com o nome de todos os teclados da Kabum!", () => {
     expect(verifyStore("KaBuM!", "keyboards")).toEqual([
       'Teclado Logitech K120, ABNT2',
@@ -29,5 +30,32 @@ describe("Exercício 5 Função nomeFrutas()", () => {
   });
   it("verifica se a função verifyStore('Mercado Livre', 'graphicsCard') retorna um array com o nome de todos os graphicsCard da Mercado Livre", () => {
     expect(verifyStore("Mercado Livre", "graphicsCard")).toEqual([]);
+  });
+});
+
+describe("Exercício 6 Função verifyId", () => {
+  it("verifica se a função verifyId('K009') retorna um objeto com os dados do produto", () => {
+    expect(verifyId("K009")).toEqual({
+      id: 'K009',
+      name: 'Teclado Gamer Trust GXT 835 Azor',
+      brand: 'Trust',
+      color: [ 'black' ],
+      bluetooth: false,
+      stores: [ 'KaBuM!', 'Americanas', 'Pichau' ],
+      prices: [ 149.9, 160.66, 180.99 ]
+    });
+  });
+  it("verifica se a função verifyId('M004') retorna um objeto com os dados do produto", () => {
+    expect(verifyId("M004")).toEqual({
+      id: "M004",
+      name: "Mouse Logitech M170",
+      brand: "Logitech",
+      dpi: 1.000,
+      color: ["black", "white", "red"],
+      bluetooth: true,
+      buttons: 3,
+      stores: ["Shopee", "Kabum!", "Kalunga"],
+      prices: [10.10, 69.90, 69.90],
+    },);
   });
 });
